66ab659b10c93f5029868f0068d3a32b7276ec68
Bob <Bob@Bobs-MacBook-Pro.local>
2015-09-28 13:45:49 +0800

    bob:

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-// HJApp 1.0/平台/设备名/DeviceID/系统版本/应用包名/AppVersion.buildVersion/渠道名/浏览器默认UA
-
-//  v1.0.0/android/XT1085/3D5D4266C605E4497B6888C/5.0.2/com.hujiang.demo.ocsplayerdownloaddemo/1.0.1/from360/Mozilla/5.0 (Linux; Android 5.0.2; XT1085 Build/LXE22.92-30) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Mobile Safari/537.36
-
-// var encodeUAStr = 'HJApp%20v1.0.0/android/XT1085/3D5D4266C605E4497B6888C/5.0.2/com.hujiang.demo.ocsplayerdownloaddemo/1.0.1/from360/Mozilla%2F5.0%20(Linux%3B%20Android%205.0.2%3B%20XT1085%20Build%2FLXE22.92-30)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Version%2F4.0%20Chrome%2F37.0.0.0%20Mobile%20Safari%2F537.36';
-
-// HJUA.init(encodeUAStr);
-
-// console.info(HJUA.getAppVer());
-// console.info(HJUA.getPlatform());
-// console.info(HJUA.getDeviceID());
-// console.info(HJUA.getSystemVer());
-// console.info(HJUA.getAppPackage());
-// console.info(HJUA.getBuildVer());
-// console.info(HJUA.getChannel());
-// console.info(HJUA.getOriginalUA());
-
 var HJUA = (function(){
     'use strict';
-    
+
     var _paramArr = [];
     var init = function(encodeUAStr) {
       var decodeUAStr = decodeURIComponent(encodeUAStr);
@@ -56,7 +39,7 @@ var HJUA = (function(){
     var getOriginalUA = function () {
       var result = '';
       var i = 7;
-      var arrlen = _paramArr.length; 
+      var arrlen = _paramArr.length;
       for ( ; i < arrlen; i++ ) {
         var subfix = (i === arrlen - 1) ? '' : '/';
         result += _paramArr[i] + subfix;
@@ -76,7 +59,7 @@ var HJUA = (function(){
       getOriginalUA: getOriginalUA
     };
 
-  
+
 })();
 
 exports.HJUA = HJUA;
